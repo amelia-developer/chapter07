6b0bb9806e1225daf44615fc92daa75325a27059
DESKTOP-SE2AMM7\6736l <1022lys6736@gmail.com>
2024-11-27 22:55:01 +0900

    kfc메인 와꾸완성

--- src/page/BottomNav.jsx
+++ src/page/BottomNav.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const BottomNav = () => {
   return (
     <>
-        {/** TODO:하는중 */}
+        <div className="bottomNavbox">
+            <ul>
+                <li><a href="#"><span>홈</span></a></li>
+                <li><a href="#"><span>리스트</span></a></li>
+                <li><a href="#"><span>딜리버리</span></a></li>
+                <li><a href="#"><span>마이페이지</span></a></li>
+            </ul>
+        </div>        
     </>
   )
 }
