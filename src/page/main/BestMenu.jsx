69c6a0d84d7a3d6a2a5287ecaaecc24f0fdee870
DESKTOP-SE2AMM7\6736l <1022lys6736@gmail.com>
2024-12-14 14:55:47 +0900

    bestMenu컴포넌트 리팩토링

--- src/page/main/BestMenu.jsx
+++ src/page/main/BestMenu.jsx
@@ -1,40 +1,28 @@
 import React, { useEffect } from 'react'
 import kfcSprite from '../../img/kfc_sprite.png'
-import { connect } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { fetchBestMenu, setLoading } from '../../redux/action'
 import { setSelectedBestMenuProductId } from '../../redux/setMenuAction'
 import { useNavigate } from 'react-router-dom'
 
-const mapStateToProps = state => { // 상태를 읽어와서 화면에 표시해야하는 경우
-    return {
-        bestMenu: state.other.bestMenu,
-        loading: state.other.loading
-    }
-}
-
-const mapDispatchtoProps = dispatch => { // dispathch는 액션을 스토어로 보내는 함수, 액션을 디스패치해서 상태를 업데이트해야하는경우
-    return {
-        fetchBestMenu: () => dispatch(fetchBestMenu()),
-        setLoading: () => dispatch(setLoading()),
-        setSelectedBestMenuProductId: bestMenuId => dispatch(setSelectedBestMenuProductId(bestMenuId))
-    }
-}
-
-const BestMenu = ({fetchBestMenu, bestMenu, setLoading, loading, setSelectedBestMenuProductId}) => {
-// console.log(`bestMenu = ${JSON.stringify(bestMenu)}`);
-
+const BestMenu = () => {
+    const dispatch = useDispatch()
     const pageNavigate = useNavigate()
 
-    useEffect(()=> {
-        fetchBestMenu(bestMenu)
-    }, [setLoading, fetchBestMenu])
+    const bestMenu = useSelector(state => state.other.bestMenu)
+    const loading = useSelector(state => state.other.loading)
 
-    if (loading) { {/**TODO:해야함*/}
-        return <div>로딩중 로딩중 로딩중</div>
-    }
+    useEffect(()=> {
+        dispatch(setLoading())
+        dispatch(fetchBestMenu(bestMenu))
+    }, [bestMenu, dispatch])
+
+    // if (loading) { {/**TODO:해야함*/}
+    //     return <div>로딩중 로딩중 로딩중</div>
+    // }
 
     const onDetailProduct = (param) => {
-        setSelectedBestMenuProductId(param)
+        dispatch(setSelectedBestMenuProductId(param))
         // navigate
         pageNavigate(`/detail?id=${param}`)
     }
@@ -50,8 +38,8 @@ const BestMenu = ({fetchBestMenu, bestMenu, setLoading, loading, setSelectedBest
                         {
                             bestMenu.map((value, idx) => {
                                 // console.log(`value = ${JSON.stringify(value)}`);
-                               return   <li key={idx}>
-                                            <a href="#" onClick={() => onDetailProduct(value.id)}>
+                                return   <li key={idx}>
+                                            <a onClick={() => onDetailProduct(value.id)}>
                                                 <img src={`/images/${value.id}.png`} alt={value.title}/> 
                                         {/**src>img로 하면 빌드과정 필요 및 빌드과정에서 변환, 그리고 import도 필요함. 
                                          * 따라서 정적파일(폰트,이미지, 아이콘)등은 public으로 넣고, 이는 빌드과정에 포함되지 않음*/}
@@ -72,4 +60,4 @@ const BestMenu = ({fetchBestMenu, bestMenu, setLoading, loading, setSelectedBest
     )
 }
 
-export default connect(mapStateToProps, mapDispatchtoProps)(BestMenu)
+export default BestMenu
